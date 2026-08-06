be9d82628e8813a66ce03d1c3589437c2e3175d5
zhanggong <zhanggong1@@sungrowpower.com>
2026-08-06 12:04:23 +0800

    test(test): 更新线标业务初始化契约

--- tests/test_panel_label_business.py
+++ tests/test_panel_label_business.py
@@ -48,6 +48,7 @@ def test_model_initialization_uses_direct_ocr_contract():
         None,
         dedup_overlap_thresh=0.6,
         cpu_fast_path=True,
+        mask_threshold=0.7,
         detection_runner=runners[0],
         orientation_runner=runners[1],
         recognition_runner=runners[2],
